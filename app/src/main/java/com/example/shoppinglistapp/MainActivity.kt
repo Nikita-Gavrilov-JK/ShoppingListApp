428f58219d26b8885e3fac8eeba6fc4022252f3d
Nikita Gavrilov <nikgavrilov0210@gmail.com>
2024-10-09 11:54:57 +0400

    Создал фрагмент  NoteFragment

--- app/src/main/java/com/example/shoppinglistapp/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglistapp/MainActivity.kt
@@ -7,20 +7,25 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.shoppinglistapp.databinding.ActivityMainBinding
+import com.example.shoppinglistapp.view.FragmentManager
+import com.example.shoppinglistapp.view.NoteFragment
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        setBottomNavListener()
     }
 
     private fun setBottomNavListener() {
         binding.botNavMenu.setOnItemSelectedListener {
             when (it.itemId) {
                 R.id.settings -> { Log.d("MYLOG", "Settings")}
-                R.id.notes -> {Log.d("MYLOG", "Notes")}
+                R.id.notes -> {
+                   FragmentManager.setFragment(NoteFragment.newInstance(), this)
+                }
                 R.id.shop_list -> {Log.d("MYLOG", "Shop_list")}
                 R.id.new_item -> {Log.d("MYLOG", "New_item")}
             }
